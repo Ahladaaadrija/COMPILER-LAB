a3ef1e9f86f3c4cbe1a2a2a2f218684b31965cbd
Ahlada A Adrija <152667720+Ahladaaadrija@users.noreply.github.com>
2026-07-08 10:13:41 +0530

    Add input.c for lexical analyzer implementation

---
+++ Cycle 1/lexical analyzer using C/input.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+
+// This is a comment
+
+int main()
+{
+    int a = 10;
+    int b = 20;
+
+    /* Multi-line
+       comment */
+
+    if(a < b)
+    {
+        a = a + b;
+    }
+
+    return 0;
+}
